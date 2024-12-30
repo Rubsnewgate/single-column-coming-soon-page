c0cb0ecc6286b5c9e7601ff2f3396c4fad7bf4ae
rubsnewgate <sart.rubi98@gmail.com>
2024-12-30 12:00:35 -0600

    refactor(sass files): update SASS syntax from .scss to .sass and migrate from @import to @use

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
-const SUBMIT_BTN = document.querySelector('.submit-btn')
+const domElements = {
+	submitBtn: document.querySelector('.submit-btn'),
+	emailInput: document.querySelector('.form__input'),
+	errorMessage: document.querySelector('.email--error'),
+}
 
-SUBMIT_BTN.addEventListener('click', (event) => {
+const validateEmail = (event) => {
 	event.preventDefault()
 
-	let email = document.querySelector('.form__input').value
+	let userEmail = domElements.emailInput.value
 	let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 
-	if (!emailPattern.test(email)) {
-		document.querySelector('.form__input').style.border = '1px solid hsl(354, 100%, 66%)'
-		document.querySelector('.email--error').style.display = 'block'
+	if (!emailPattern.test(userEmail)) {
+		domElements.emailInput.style.border = '1px solid hsl(354, 100%, 66%)'
+		domElements.errorMessage.style.display = 'block'
 	}
 	else {
-		document.querySelector('.email--error').style.display = 'none'
+		domElements.errorMessage.style.display = 'none'
 	}
-})
+}
+
+domElements.submitBtn.addEventListener('click', validateEmail)
